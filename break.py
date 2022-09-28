eccc650c1cf55205f49dba672f8a4d524438ebc4
liangzyayw <liangzyayw@163.com>
2022-09-28 17:43:28 +0800

    计算机更改为计算器

--- break.py
+++ break.py
@@ -1,5 +1,16 @@
+A = ['(输入 "q" 退出)']
+
 while True:
-    Pen = input("请输入文字：")
-    if Pen == "q":
+    Pen = input("请输入文字{}:".format(A[0]))           ## {} .format(A[0])  在"" 里面使用变量
+    # Pen = eval(Pen)
+    # try:
+    #     Pen=eval(Pen)
+    # except NameError:
+    #     Pen=Pen
+    # else:
+    #     Pen=eval(Pen)
+    if Pen == "q":                                    ## 判断输入的数据是否为 q  q==退出
         break
-print(Pen)
+    elif type(Pen) == int:
+        continue                                      ## 结束循环 退出代码
+    print(Pen)
